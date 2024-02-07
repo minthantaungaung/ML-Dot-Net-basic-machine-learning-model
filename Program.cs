880f75df7920672de38f1c7fab391f753601a7c9
minthant_aungaung <minthant.aungaung@kbzbank.com>
2024-02-07 10:41:38 +0630

    :tada:

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+//Load sample data
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = @"Crust is not bad.",
+};
+
+//Load model and predict output
+var result = SentimentModel.Predict(sampleData);
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}"); 
